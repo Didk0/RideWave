85ac8bed561eeeb6d7f476a99a5b3bd3965da2ab
deyan.tsvetkow <deyan.tsvektow.external@indra-avitech.aero>
2025-09-06 09:31:09 +0300

    First commit

---
+++ rider/src/test/java/com/ride/wave/rider/rider/RiderApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ride.wave.rider.rider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiderApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
