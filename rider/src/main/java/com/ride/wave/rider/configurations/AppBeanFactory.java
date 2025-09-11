54ac1b22ca2cb83635deb87d3ec49bfb7b116c75
deyan.tsvetkow <deyan.tsvektow.external@indra-avitech.aero>
2025-09-11 13:05:11 +0300

    Add config server. Move configurations to config server. Add event bus for dynamic configs refresh. Add h2 for test. Add rabbitmq to docker-compose file.

--- rider/src/main/java/com/ride/wave/rider/configurations/AppBeanFactory.java
+++ rider/src/main/java/com/ride/wave/rider/configurations/AppBeanFactory.java
@@ -1,11 +1,11 @@
-package com.ride.wave.driver.configurations;
+package com.ride.wave.rider.configurations;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class BeanFactory {
+public class AppBeanFactory {
 
   @Bean
   public ModelMapper modelMapper() {
